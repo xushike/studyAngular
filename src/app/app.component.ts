51733a5dec220e45501fb814fb08ab099deedcf9
许世科 <xushike@xushikedeMacBook-Pro-2.local>
2018-01-01 23:55:53 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = '';
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.title = 'hello';
+    }, 2000);
+  }
+  // title = 'My First Angular App!! ';
+
 }
