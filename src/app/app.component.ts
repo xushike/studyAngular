a78980f38e9a13d6d79503eb031cd6ea2fe12e00
许世科 <xushike@xushikedeMacBook-Pro-2.local>
2018-01-04 22:11:12 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,17 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = '';
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.title = 'hello';
-    }, 2000);
-  }
-  // title = 'My First Angular App!! ';
-
+export class AppComponent  {
+  // ngOnInit(): void {
+  //   setTimeout(() => {
+  //     this.title = 'hello';
+  //   }, 2000);
+  // }
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.title = 'hello';
+  //   }, 2000);
+  // }
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
