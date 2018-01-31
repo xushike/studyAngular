38d2fdc7a551556c454c17e3fbe36361471aaf2b
xuxiaoke <992124325@qq.com>
2018-01-31 23:57:47 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  // ngOnInit(): void {
-  //   setTimeout(() => {
-  //     this.title = 'hello';
-  //   }, 2000);
-  // }
-  // constructor() {
-  //   setTimeout(() => {
-  //     this.title = 'hello';
-  //   }, 2000);
-  // }
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
